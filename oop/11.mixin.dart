a7930e7c66b9828c888da2af47732a74a308a69f
xueming <810505339@qq.com>
2023-04-25 03:18:03 +0800

    edit mixin

--- oop/11.mixin.dart
+++ oop/11.mixin.dart
@@ -1,3 +1,22 @@
+//混入解决多继承?
+mixin MixinA {
+  String name = 'mixinA';
+  printA() {
+    print('A');
+  }
+}
+
+mixin MixinB {
+  String name = 'mixinB';
+  printB() {
+    print('B');
+  }
+}
+
+class MyClass with MixinA, MixinB {}
+
 void main(List<String> args) {
-  
+  var c = new MyClass();
+  c.printA();
+  c.printB();
 }
